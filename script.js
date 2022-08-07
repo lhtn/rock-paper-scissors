17c3724beba881543df156ce3c5457e549bcc34b
lhtn <lily.nguyen1129@gmail.com>
2022-08-06 20:36:09 -0400

    Add prompt, case-insensitive

--- script.js
+++ script.js
@@ -7,29 +7,32 @@ function getComputerChoice() {
 
 // define a function named playRound which takes two parameters playerSelection and computerSelection
 // and returns a string that declares the winner of the round such as "You Lose! Paper beats Rock"
+// make playerSelection parameter case-insensitive
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Paper") {
+    if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
         return "You lose! Paper beats Rock.";
-    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         return "You win! Rock beats Scissors.";
-    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
         return "You win! Paper beats Rock.";
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
         return "You lose! Scissors beats Paper.";
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "rock") {
         return "You lose! Rock beats Scissors.";
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
         return "You win! Scissors beats Paper.";
     } else {
         return "It's a tie! Try again!";
     }
 }
 
-const playerSelection = "Rock";
+
+// use prompt() to get input from the user
+let playerSelection = prompt("Please enter Rock, Paper, or Scissors");
 const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
 
-// define a function called game()
-// call the playRound function
-// which plays a 5 round game that keeps score
+
+// define a function called game
+// call the playRound function which plays a 5 round game that keeps score
 // report a winner or loser at the end
